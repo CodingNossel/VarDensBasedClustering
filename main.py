617a4b18904efb80d3465b5d1bc0f5652ce8a4b4
leucht <gfpinthehouse@gmail.com>
2024-01-14 19:28:07 +0100

    Create NOT Edge-Quantile-Cut

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import HDBSCAN
 from sklearn.datasets import make_blobs
 from sklearn import datasets
+
 # TODO: add way to load different datasets
 centers = [[1, 1], [-1, -1], [1, -1], [20, 20], [20, 21], [21, 20], [21, 21]]
 # blobies
@@ -23,10 +24,10 @@ X, labels_true = datasets.load_digits(
 # )
 
 # run HDBSCAN on Data. Possible to change parameters here
-#hdb = HDBSCAN(store_centers='both')
+# hdb = HDBSCAN(store_centers='both')
 hdb = HDBSCAN(store_centers='both', min_cluster_size=10, min_samples=1)
 hdb.fit(X)
-#print iris for the test
+# print iris for the test
 print(hdb.labels_)
 
 print("------------------")
@@ -39,46 +40,55 @@ newData = copy.deepcopy(data)
 # Create hierarchy from centroids and pass to tree for further use. Possible to change parameters here
 z = hierarchy.linkage(hdb.centroids_, method='single')
 
-#linkage to IDFKIKMS
+# linkage to IDFKIKMS
 # get height of parent and child cluster
-#TODO: figure out how to get points for cluster (z[i][0] and z[i][1])
-#print(z)
+# TODO: figure out how to get points for cluster (z[i][0] and z[i][1])
+# print(z)
 cutlist = []
 for i in range(len(z) - 1):
     height = z[i][2]
-    heightChild = z[i+1][2]
+    heightChild = z[i + 1][2]
     heightDiff = height - heightChild
-    #TODO GET POINTS OF U
-    #uPoints = [data.map(lambda x: x['label']).filter(lambda x: x == z[i][0] or x == z[i][1])]
-    uPoints = []
-    
-    cutlist.append((heightDiff, uPoints,  height))
-#quantile = 0.95
-#[0, 0, 1, 505, 10]
-#[505, 10, 1, 0, 0]
-#print(cutlist)
+
+    cutlist.append((heightDiff, height))
+
+
+# quantile = 0.95
+# [0, 0, 1, 505, 10]
+# [505, 10, 1, 0, 0]
+# print(cutlist)
 # quantile must be up 1.0
 def get_biggest_density_change(cutlist, quantile):
     cutlist.sort(key=lambda tup: tup[0])
-    #cut from the point where quantile is reached
+    # cut from the point where quantile is reached
     cutlist = cutlist[:int(len(cutlist) * quantile)]
     return cutlist
-list = get_biggest_density_change(cutlist, 0.55)
-result = copy.deepcopy(newData)
-# debug why label assigment is wrong
-for i in range(len(list)):
-        points = []
-        for element in newData:
-            if (element['label'] == list[i][0]):
-                result['label'] = list[i][1]
 
-sorted(list, key=lambda tup: tup[2])
-#hierarchyTreeCentroids = hierarchy.to_tree(z)
+
+list2 = get_biggest_density_change(cutlist, 0.55)
+result = copy.deepcopy(newData)
+
+# debug why label assigment is wrong
+
+for element in result:
+    for i in range(len(list2)):
+        val = list2[i][1]
+        print('value: ', val)
+        for j in range(len(z)):
+            comp = z[j][2]
+            print('compare: ', comp)
+            if val == comp:
+                cluster = z[j][0]
+                print('changed from: ', element['label'], '->', 'to: ', cluster, )
+                element['label'] = cluster
+
+sorted(list2, key=lambda tup: tup[1])
+# hierarchyTreeCentroids = hierarchy.to_tree(z)
 
 # Create hierarchy from centroids and pass to tree for further use. Possible to change parameters here
-#h = hierarchy.linkage(hdb.medoids_, method='single')
+# h = hierarchy.linkage(hdb.medoids_, method='single')
 
-#hierarchyTreeMedoids = hierarchy.to_tree(h)
+# hierarchyTreeMedoids = hierarchy.to_tree(h)
 
 # TODO: use whichever fits get for getting the coordinates
 # https://docs.scipy.org/doc/scipy/reference/cluster.hierarchy.html#module-scipy.cluster.hierarchy
@@ -109,13 +119,9 @@ sorted(list, key=lambda tup: tup[2])
 #      if (count == 10):
 #           break
 
-for i in range(len(data)):
-    # added, removed, modified, same = dict_compare(data[i], result[i])
-    # print("added: ", added, "removed: ", removed, "modified: ", modified, "same: ", same)
-     print(all(data[i]) == all(result[i]))
+# for i in range(len(data)):
+# print('data: ', data[i],'compare_result: ',  data[i]['label'] == result[i]['label'])
 
-#plt.scatter([newList['coord'][:] for newList in result], y=[newList['coord'][:] for newList in result], c=[newList['label'] for newList in result])
-#dendrogram = hierarchy.dendrogram(z)
-#plt.show()
-
-
+# plt.scatter([newList['coord'][:] for newList in result], y=[newList['coord'][:] for newList in result], c=[newList['label'] for newList in result])
+# dendrogram = hierarchy.dendrogram(z)
+# plt.show()
