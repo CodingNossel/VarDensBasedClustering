94ac9ba8ca13676a25175a3c46da184108d510ae
Timur Sinnurow <timur.sinnurow@st.ovgu.de>
2024-01-14 17:45:14 +0100

    amogus

--- main.py
+++ main.py
@@ -9,17 +9,27 @@ from sklearn.datasets import make_blobs
 from sklearn import datasets
 # TODO: add way to load different datasets
 centers = [[1, 1], [-1, -1], [1, -1], [20, 20], [20, 21], [21, 20], [21, 21]]
+# blobies
 # X, labels_true = make_blobs(
 #     n_samples=750, centers=centers, cluster_std=0.4, random_state=0
 # )
+# digits
 X, labels_true = datasets.load_digits(
     return_X_y=True
 )
+# iris
+# X, labels_true = datasets.load_iris(
+#     return_X_y=True
+# )
+
 # run HDBSCAN on Data. Possible to change parameters here
 #hdb = HDBSCAN(store_centers='both')
 hdb = HDBSCAN(store_centers='both', min_cluster_size=10, min_samples=1)
 hdb.fit(X)
+#print iris for the test
+print(hdb.labels_)
 
+print("------------------")
 data = [dict(index=index, label=hdb.labels_[index], coord=value) for index, value in enumerate(X)]
 
 data.sort(key=lambda x: x['label'])
@@ -32,7 +42,7 @@ z = hierarchy.linkage(hdb.centroids_, method='single')
 #linkage to IDFKIKMS
 # get height of parent and child cluster
 #TODO: figure out how to get points for cluster (z[i][0] and z[i][1])
-print(z)
+#print(z)
 cutlist = []
 for i in range(len(z) - 1):
     height = z[i][2]
@@ -46,7 +56,7 @@ for i in range(len(z) - 1):
 #quantile = 0.95
 #[0, 0, 1, 505, 10]
 #[505, 10, 1, 0, 0]
-print(cutlist)
+#print(cutlist)
 # quantile must be up 1.0
 def get_biggest_density_change(cutlist, quantile):
     cutlist.sort(key=lambda tup: tup[0])
@@ -55,6 +65,7 @@ def get_biggest_density_change(cutlist, quantile):
     return cutlist
 list = get_biggest_density_change(cutlist, 0.55)
 result = copy.deepcopy(newData)
+# debug why label assigment is wrong
 for i in range(len(list)):
         points = []
         for element in newData:
@@ -62,8 +73,6 @@ for i in range(len(list)):
                 result['label'] = list[i][1]
 
 sorted(list, key=lambda tup: tup[2])
-for i in range(len(list)):
-    print(list[i][2])
 #hierarchyTreeCentroids = hierarchy.to_tree(z)
 
 # Create hierarchy from centroids and pass to tree for further use. Possible to change parameters here
@@ -85,10 +94,28 @@ for i in range(len(list)):
 # outPutlist = []
 # for element in result:
 #     plt.scatter(element['coord'][0], element['coord'][1], c=element['label'])
+# count = 0
+# for a in result:
+#      print(a['label'], "coord:",  a['coord'], "index:", a['index'], "count:", count)
+#      count += 1
+#      if (count == 10):
+#           break
 
-for a in result:
-     print(a['coord'])
+# print("------------------")
+# count = 0
+# for i in data:
+#      print(i['label'], "coord:",  i['coord'], " index:", i['index'], "count:", count)
+#      count += 1
+#      if (count == 10):
+#           break
 
-plt.scatter([newList['coord'][:] for newList in result], y=[newList['coord'][:] for newList in result], c=[newList['label'] for newList in result])
+for i in range(len(data)):
+    # added, removed, modified, same = dict_compare(data[i], result[i])
+    # print("added: ", added, "removed: ", removed, "modified: ", modified, "same: ", same)
+     print(all(data[i]) == all(result[i]))
+
+#plt.scatter([newList['coord'][:] for newList in result], y=[newList['coord'][:] for newList in result], c=[newList['label'] for newList in result])
 #dendrogram = hierarchy.dendrogram(z)
-plt.show()
+#plt.show()
+
+
